38d0d2305a116cde5d9df86daa4009961b7798aa
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-04-29 21:29:12 -0400

    feat: add OR syntax

--- src/utils.js
+++ src/utils.js
@@ -1,28 +1,37 @@
 function parseGrep(s) {
-  const parsed = s.split('+').map((tag) => {
-    if (tag.startsWith('-')) {
-      return {
-        tag: tag.slice(1),
-        invert: true,
+  // top level split - using space, each part is OR
+  const ORS = s.split(' ').map((part) => {
+    // now every part is an AND
+    const parsed = part.split('+').map((tag) => {
+      if (tag.startsWith('-')) {
+        return {
+          tag: tag.slice(1),
+          invert: true,
+        }
       }
-    }
 
-    return {
-      tag,
-      invert: false,
-    }
+      return {
+        tag,
+        invert: false,
+      }
+    })
+
+    return parsed
   })
 
-  return parsed
+  return ORS
 }
 
 function shouldTestRun(parsedGrep, testName) {
-  return parsedGrep.every((p) => {
-    if (p.invert) {
-      return !testName.includes(p.tag)
-    }
+  // top levels are OR
+  return parsedGrep.some((orPart) => {
+    return orPart.every((p) => {
+      if (p.invert) {
+        return !testName.includes(p.tag)
+      }
 
-    return testName.includes(p.tag)
+      return testName.includes(p.tag)
+    })
   })
 }
 
